13c72c333eac3791b89906601a9b3f39d05dcf4d
Jake Lee <jake.jklee425@gmail.com>
2020-05-20 15:13:46 -0700

    updated redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,22 @@
 import { createStore } from 'redux'
+import { uuid } from 'uuidv4'
 import reducer from './reducer'
 
+const MAX_HEIGHT = 600
 const INITIAL_STATE = {
-  arr: [...Array(8)].map(() => {
+  arr: [...Array(3)].map(() => {
     return {
-      height: Math.floor(Math.random() * 30 * 18 + 20),
-      color: 'lightblue'
+      height: Math.floor(Math.random() * MAX_HEIGHT + 20),
+      key: uuid()
     }
   }),
   algorithm: null,
   isSorting: false
 }
-const store = createStore(reducer, INITIAL_STATE)
+const store = createStore(
+  reducer,
+  INITIAL_STATE,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
