a0e8584eed9d6285e8f294e5c3465a76f40df74b
Jake Lee <jake.jklee425@gmail.com>
2020-06-17 21:37:41 -0700

    added animation

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -1,13 +1,13 @@
 export const bubbleSort = arr => {
   if (arr.length < 2) return arr
-
+  console.log('bubble arr', arr)
   let animations = []
   let temp = [...arr]
   for (let i = 0; i < temp.length; i++) {
-    for (let j = i + 1; j < temp.length; j++) {
-      if (temp[i] > temp[j]) {
-        animations.push([i, j])
-        ;[temp[i], temp[j]] = [temp[j], temp[i]]
+    for (let j = 0; j < temp.length - 1; j++) {
+      if (temp[j].width > temp[j + 1].width) {
+        animations.push([j, j + 1])
+        ;[temp[j], temp[j + 1]] = [temp[j + 1], temp[j]]
       }
     }
   }
