69c431987b5dc7a2283a4ea38db668283f2e1864
Jake Lee <jake.jklee425@gmail.com>
2020-06-17 21:38:11 -0700

    added animation

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -1,19 +1,23 @@
 export const insertionSort = arr => {
   if (arr.length < 2) return arr
   // assume the array is sorted when there is only one element
-  for (let i = 1; i < arr.length; i++) {
+  let temp = [...arr],
+    animations = []
+
+  for (let i = 1, length = temp.length; i < length; i++) {
     // current element is bigger than the one on left
-    if (arr[i] < arr[i - 1]) {
+    if (temp[i].width < temp[i - 1].width) {
       // find a right pos from the rightmost pos to insert in sorted space
       for (let j = i; j >= 0; j--) {
-        if (arr[j - 1] > arr[j]) {
-          ;[arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
+        if (temp[j - 1]?.width > temp[j].width) {
+          animations.push([j - 1, j])
+          ;[temp[j - 1], temp[j]] = [temp[j], temp[j - 1]]
         }
       }
     }
   }
-
-  return arr
+  console.log('insertion sort animations', arr, animations)
+  return animations
 }
 
 // module.exports = insertionSort
