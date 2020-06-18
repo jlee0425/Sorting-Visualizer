40cfd9a465d200bd7a4c6d80b483c6e4c6da3c80
Jake Lee <jake.jklee425@gmail.com>
2020-06-17 21:37:18 -0700

    added useContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import Header from './components/Header'
-import MenuBar from './components/MenuBar'
+import ToolBar from './components/ToolBar'
 import Chart from './components/Chart'
+
+import AppContext from './AppContext'
 import './App.scss'
 
-function App () {
+const App = () => {
+  const [arr, setArray] = useState([])
+  const [algorithm, setAlgorithm] = useState(null)
+  const [running, setRunning] = useState(false)
+
+  const value = useMemo(
+    () => ({
+      arr,
+      setArray,
+      algorithm,
+      setAlgorithm,
+      running,
+      setRunning
+    }),
+    [arr, setArray, algorithm, setAlgorithm, running, setRunning]
+  )
   return (
     <div className='container'>
       <Header />
-      <MenuBar />
-      <Chart />
+      <AppContext.Provider value={value}>
+        <ToolBar />
+        <Chart />
+      </AppContext.Provider>
     </div>
   )
 }
