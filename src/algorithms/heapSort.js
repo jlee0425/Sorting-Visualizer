0810f3183ca5409a326f84b25ff1d5b4b6eeff07
Jake Lee <jake.jklee425@gmail.com>
2020-06-17 21:37:55 -0700

    added animation

--- src/algorithms/heapSort.js
+++ src/algorithms/heapSort.js
@@ -1,35 +1,37 @@
-export const heapSort = arr => {
-  // build maxheap
-  for (let i = Math.floor(arr.length / 2); i >= 0; i--) {
-    heapify(arr, i)
-  }
+import { CircularProgress } from '@material-ui/core'
 
-  // swap and build maxheap
-  for (let i = arr.length - 1; i > 0; i--) {
-    ;[arr[0], arr[i - 1]] = [arr[i - 1], arr[0]]
-    heapify(arr, 0)
-  }
-  return arr
-}
-
-const heapify = (arr, i) => {
+let animations = []
+const heapify = (arr, i, length = arr.length) => {
   let left = 2 * i + 1,
     right = 2 * i + 2,
-    curMaxPos = i,
-    length = arr.length
+    curMaxPos = i
 
-  if (left < length && arr[left] > arr[curMaxPos]) {
+  if (left < length && arr[left].width > arr[curMaxPos].width) {
     curMaxPos = left
   }
 
-  if (right < length && arr[right] > arr[curMaxPos]) {
+  if (right < length && arr[right].width > arr[curMaxPos].width) {
     curMaxPos = right
   }
 
   if (curMaxPos !== i) {
+    // animations.push([i, curMaxPos])
     ;[arr[i], arr[curMaxPos]] = [arr[curMaxPos], arr[i]]
-    heapify(arr, curMaxPos)
+    heapify(arr, curMaxPos, length)
   }
+}
+export const heapSort = arr => {
+  for (let i = Math.floor(arr.length / 2); i >= 0; i--) {
+    heapify(arr, i)
+  }
+  for (let i = arr.length - 1; i >= 0; i--) {
+    animations.push([0, i])
+    ;[arr[0], arr[i]] = [arr[i], arr[0]]
+    heapify(arr, 0, i)
+  }
+  console.log('after sort arr', arr)
+  console.log('animations', animations)
+  return animations
 }
 
 // module.exports = heapSort
