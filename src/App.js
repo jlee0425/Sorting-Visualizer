4b2b8cc5baf7774f9c7a07fcf4a768c2d1296884
Jake Lee <jake.jklee425@gmail.com>
2020-06-27 19:47:48 -0700

    fixed props

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
+
 import Header from './components/Header'
 import ToolBar from './components/ToolBar'
-import Chart from './components/Chart'
+import AnimatedChart from './components/Chart'
 
 import AppContext from './AppContext'
+import { generateRandomArray } from './components/ToolBar/generateArray'
 import './App.scss'
 
 const App = () => {
-  const [arr, setArray] = useState([])
+  const [arr, setArray] = useState(generateRandomArray(8))
   const [algorithm, setAlgorithm] = useState(null)
+  const [animations, setAnimations] = useState([])
+  const [sorted, setSorted] = useState(false)
   const [running, setRunning] = useState(false)
-
+  useEffect(() => {
+    setAnimations([])
+    const fetchAnimations = () => algorithm(arr)
+    if (algorithm && !sorted) setAnimations(fetchAnimations())
+  }, [arr, algorithm, setRunning, sorted])
   const value = useMemo(
     () => ({
       arr,
       setArray,
       algorithm,
       setAlgorithm,
+      sorted,
+      setSorted,
       running,
       setRunning
     }),
-    [arr, setArray, algorithm, setAlgorithm, running, setRunning]
+    [
+      arr,
+      setArray,
+      algorithm,
+      setAlgorithm,
+      sorted,
+      setSorted,
+      running,
+      setRunning
+    ]
   )
   return (
     <div className='container'>
       <Header />
       <AppContext.Provider value={value}>
         <ToolBar />
-        <Chart />
+        <AnimatedChart arr={arr} animations={animations} />
       </AppContext.Provider>
     </div>
   )
