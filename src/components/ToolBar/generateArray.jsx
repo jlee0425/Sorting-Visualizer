4a227bc895d52a9a137b8691120285771a82e428
Jake Lee <jake.jklee425@gmail.com>
2020-06-29 02:42:06 -0700

    changed to semantic-ui

--- src/components/ToolBar/generateArray.jsx
+++ src/components/ToolBar/generateArray.jsx
@@ -1,26 +1,26 @@
-import React, { useContext } from 'react'
-import { uuid } from 'uuidv4'
+import React, { useContext, useEffect, useState } from 'react'
+import { Button } from 'semantic-ui-react'
 
+import { generateRandomArray, resizeHandler } from './helper'
 import AppContext from '../../AppContext'
 
-export const generateRandomArray = size => {
-  const MAX_WIDTH = 80
-  return [...Array(size)].map(() => {
-    return {
-      width: `${Math.floor(Math.random() * MAX_WIDTH + 10)}%`,
-      key: uuid()
-    }
-  })
-}
 const NewArrayButton = () => {
   const { arr, setArray } = useContext(AppContext)
+  const [buttonSize, setButtonSize] = useState('large')
   const handleButtonClick = () => {
     setArray(generateRandomArray(arr.length))
   }
-
+  useEffect(() => {
+    const handleResize = () => {
+      setButtonSize(resizeHandler())
+    }
+    window.addEventListener('resize', handleResize)
+  })
   return (
     <React.Fragment>
-      <button onClick={handleButtonClick}>New Array</button>
+      <Button color='blue' onClick={handleButtonClick} size={buttonSize}>
+        New <br /> Array
+      </Button>
     </React.Fragment>
   )
 }
